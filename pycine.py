46b89c774606c0a3f9eb7e4eb313b8a523260da7
fscheidt <felippescheidt@gmail.com>
2023-09-04 22:00:57 -0300

    exemplo pycine fastapi e tmdb

--- pycine.py
+++ pycine.py
@@ -2,10 +2,31 @@ from fastapi import FastAPI
 from tmdb import get_json
 app = FastAPI()
 
+# ========================================================
+
+# ATIVIDADE 1
+
 @app.get("/filme/{title}")
 async def find_movie(title: str):
-    """  """
-    pass
+    """ 
+    procura filmes pelo titulo e ordena pelos mais populares 
+    Exemplo: /filme/avatar
+    """
+    return {"title": title}
+
+# ========================================================
+
+# ATIVIDADE 2
+
+@app.get("/artista/filmes")
+async def find_filmes_artista(personId: int):
+    """ 
+    busca os filmes mais populares de um artista 
+    Exemplo: /artista/filmes?personId=1100
+    """
+    return {"id": personId}
+
+# ========================================================
 
 @app.get("/artista/{name}")
 async def get_artista(name: str):
@@ -23,7 +44,7 @@ async def get_artista(name: str):
             'name': artist['name'],
             'rank': artist['popularity']
         })
-    # ordenar filtro pelo atributo rank
+    # ordenar lista de artistas (filtro) pelo atributo rank
     filtro.sort(reverse=True, key=lambda artist:artist['rank'])
     # return data
     return filtro
